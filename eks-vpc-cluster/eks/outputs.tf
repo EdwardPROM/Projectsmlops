e036cd1399f8ac7240b1588ad75a54ae9fdf1d78
EdwardPROM <eduardkoles@gmail.com>
2025-10-29 12:16:31 +0200

    Add .gitignore and remove .terraform from repo

---
+++ eks-vpc-cluster/eks/outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group for EKS cluster"
+  value       = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
