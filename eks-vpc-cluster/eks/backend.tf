9405679141843a4b36283037e072d28ded9ce735
EdwardProm <eduardkoles@gmail.com>
2025-10-29 12:42:37 +0200

    Delete eks-vpc-cluster directory

--- eks-vpc-cluster/eks/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate-edward-mlops"
-    key            = "eks/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- eks-vpc-cluster/vpc/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate-edward-mlops"       # <-- ОНОВЛЕНА НАЗВА
-    key            = "vpc/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
